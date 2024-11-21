ff70c698a0ceae735b75336d9d78595b3e2b44be
NicolasPiron <ncls.piron@gmail.com>
2024-11-21 17:41:09 +0100

    minor

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data = pd.read_excel('data/code_animaux.xlsx')
-
-print(data.head())
-print(data.columns)
-print(data['Sexe'])
-print(data['numero'])
-print(data['Lignée / Souche (Nom)'])
