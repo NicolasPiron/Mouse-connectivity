82517bc1841073cd03dd5b1b9cd0ae18062ac92e
NicolasPiron <ncls.piron@gmail.com>
2024-11-26 14:07:07 +0100

    minor

--- full_pipeline.sh
+++ full_pipeline.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
 
-# This script runs the Python scripts plot_av.py and stats.py, and the bash script dl_results.sh.
-# dl_results.sh is only run if the script is executed on the local machine.
-
-# Structure of dl_results.sh:
-# =================================
-    # #!/bin/bash
-    # Define the remote and local directories
-    # REMOTE_USER=""        
-    # REMOTE_HOST="" 
-    # REMOTE_DIR="" 
-    # LOCAL_DIR=""  
-    # echo "Starting rsync transfer from $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR to $LOCAL_DIR"
-
-    # # get the latest files
-    # rsync -avz --progress "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" "$LOCAL_DIR"
-
-    # # check if rsync was successful
-    # if [ $? -eq 0 ]; then
-    #     echo "Rsync completed successfully!"
-    # else
-    #     echo "Rsync encountered an error."
-    # fi
-# =================================
+# This script runs the Python scripts plot_av.py and stats.py
 
 check_environment() {
     if [[ "$(hostname)" == "MacBook-Pro-de-Nisso-6.local" ]]; then
-        echo "Activating local Conda environment 'pulsation_proj'."
+        echo "Activating local Conda environment 'mconn'."
         conda init bash
         conda activate pulsation_proj
     else
@@ -54,12 +32,33 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-if [[ "$(hostname)" == "MacBook-Pro-de-Nisso-6.local" ]]; then
+# if [[ "$(hostname)" == "MacBook-Pro-de-Nisso-6.local" ]]; then
 
-    echo "Running dl_results.sh..."
-    ./dl_results.sh
-    if [ $? -ne 0 ]; then
-        echo "Error: dl_results.sh failed."
-        exit 1
-    fi
-fi
+#     echo "Running dl_results.sh..."
+#     ./dl_results.sh
+#     if [ $? -ne 0 ]; then
+#         echo "Error: dl_results.sh failed."
+#         exit 1
+#     fi
+# fi
+
+# Structure of dl_results.sh:
+# =================================
+    # #!/bin/bash
+    # Define the remote and local directories
+    # REMOTE_USER=""        
+    # REMOTE_HOST="" 
+    # REMOTE_DIR="" 
+    # LOCAL_DIR=""  
+    # echo "Starting rsync transfer from $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR to $LOCAL_DIR"
+
+    # # get the latest files
+    # rsync -avz --progress "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" "$LOCAL_DIR"
+
+    # # check if rsync was successful
+    # if [ $? -eq 0 ]; then
+    #     echo "Rsync completed successfully!"
+    # else
+    #     echo "Rsync encountered an error."
+    # fi
+# =================================
